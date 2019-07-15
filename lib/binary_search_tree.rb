76caa292882aa63d771a802159f2e8335d0609d4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-15 09:43:48 -0600

    Add test for insert method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,2 +1,20 @@
 class BinarySearchTree
+  attr_reader :root
+
+  def initialize
+    @root = nil
+  end
+
+  def insert(value, data)
+    if @root.nil? 
+      @root = Node.new(value, data)
+      0
+    else
+      insert_below(@root, value, data)
+    end
+  end
+
+  def insert_below(node, value, data)
+    
+  end
 end
--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -8,4 +8,16 @@ class BinarySearchTreeTest < Minitest::Test
   def test_it_exists
     assert_instance_of BinarySearchTree, @tree
   end
+
+  def test_it_starts_with_no_root
+    assert_nil @tree.root
+  end
+  
+  def test_it_inserts_nodes
+    assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 61, @tree.root.value
+    assert_equal 1, @tree.insert(16, "Johnny English")
+    assert_equal 1, @tree.insert(92, "Sharknado 3")
+    assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
+  end
 end
