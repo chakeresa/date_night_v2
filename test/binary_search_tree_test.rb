a79e49c73e4de2b68f2c6295424231628d8b2f9c
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-15 10:00:32 -0600

    Add additional assertions to verify insert is working

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -16,8 +16,14 @@ class BinarySearchTreeTest < Minitest::Test
   def test_it_inserts_nodes
     assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
     assert_equal 61, @tree.root.value
+
     assert_equal 1, @tree.insert(16, "Johnny English")
+    assert_equal 16, @tree.root.left.value
+    
     assert_equal 1, @tree.insert(92, "Sharknado 3")
+    assert_equal 92, @tree.root.right.value
+    
     assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
+    assert_equal 50, @tree.root.left.right.value
   end
 end
